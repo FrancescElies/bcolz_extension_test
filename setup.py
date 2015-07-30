d217c00256bd580e79253c02c74fd80b19d46f8c
Francesc Elies Henar <felies@visualfabriq.com>
2015-07-30 10:39:31 +0200

    setup

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 from numpy.distutils.misc_util import get_numpy_include_dirs
 
+# Sources & libraries
+inc_dirs = ['my_extension']
+lib_dirs = []
+libs = []
+def_macros = []
+sources = ["my_extension/carray_ext.pyx"]
+
+# Include NumPy header dirs
+from numpy.distutils.misc_util import get_numpy_include_dirs
+inc_dirs.extend(get_numpy_include_dirs())
+optional_libs = []
 
 # Sources
-sources = ["my_extension/carray_ext.pyx"]
 
 setup(
     name="my_package",
@@ -13,7 +23,11 @@ setup(
     ext_modules=[
         Extension(
             "my_extension.carray_ext",
+            include_dirs=inc_dirs,
+            define_macros=def_macros,
             sources=sources,
+            library_dirs=lib_dirs,
+            libraries=libs,
         ),
     ],
     cmdclass={"build_ext": build_ext},
